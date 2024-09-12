e789ce4bd45d5c19a8a60b830be956c07c564d70
Om-Govindani <omgovindani@gmail.com>
2024-09-12 12:51:53 +0530

    changing the baseURL in frontend

--- frontend/src/Utils/axiosInstance.js
+++ frontend/src/Utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://task-xi-beryl.vercel.app/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
